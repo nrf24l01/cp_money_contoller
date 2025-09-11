f05bca1136afedd0a27d9a3deba31f5090ac63d8
nrf24l01 <s.nevrovsky@ya.ru>
2025-09-11 21:24:54 +0300

    Refactor inject_captcha_solver to load the correct captcha solver extension

--- worker/tasks/inject_captcha_solver.py
+++ worker/tasks/inject_captcha_solver.py
@@ -2,7 +2,6 @@ from selenium.webdriver.chrome.options import Options
 import os
 
 def inject_captcha_solver(options: Options):
-    extension_path = os.path.join(os.path.dirname(__file__), "../booster")
-    options.add_argument("--load-extension=" + extension_path)
-    print(extension_path)
+    extension_path = os.path.join(os.path.dirname(__file__), "../captcha_solver.crx")
+    options.add_extension(extension_path)
     return options
